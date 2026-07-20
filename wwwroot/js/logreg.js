af27ac03cf9b14fe0aecd8bab15dc3baf50b0681
Hazzel Jimenez <Hazzel Jimenez@EY>
2026-07-20 22:29:01 +0800

    with Act 5, bawlang sakto ba

--- wwwroot/js/logreg.js
+++ wwwroot/js/logreg.js
@@ -1,18 +1,4 @@
 ﻿$(function () {
-    $(document).on('click', '#btnLogin', function (e) {
-        e.preventDefault();
-        $.ajax({
-            url: "/Home/Login",
-            type: "POST",
-            data: {
-                email: $('#email').val(),
-                password: $('#password').val()
-            },
-            success: function (response) {
-                alert(response);
-            }
-        })
-    })
 
     $(document).on('click', '#btnRegister', function (e) {
         e.preventDefault();
